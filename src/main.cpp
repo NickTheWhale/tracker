d2222db3f070c8defcd6086bdb6688af7ca68bf7
NickTheWhale <loehrketurkey7@gmail.com>
2023-02-08 13:46:47 -0600

    accel driver

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,13 @@
 #include <Arduino.h>
+#include <SPI.h>
 
-void setup() {
-  Serial.begin(115200);
-}
+#define ACCEL_CS 5
+#define ACCEL_SPI_CLOCK 5000000
+uint32_t n = 5000000;
+
+void setup() { Serial.begin(115200); }
 
 void loop() {
-  Serial.println(micros());
-  delay(1000);
+    Serial.println(n);
+    delay(1000);
 }
