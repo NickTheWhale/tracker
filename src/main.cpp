b86598c7e032c3d9b7d49058bc1552cf812105c0
NickTheWhale <loehrketurkey7@gmail.com>
2023-02-13 23:05:02 -0600

    lllllllora

--- src/main.cpp
+++ src/main.cpp
@@ -24,14 +24,30 @@ Adafruit_ADXL343 imu(IMU_CS, &SPI, 1);
 #define TX_POWER (20)
 #define CRC (true)
 
+// Prototypes
+void printAccel(sensors_event_t *evt);
+
 void setup() {
     Serial.begin(9600); 
-    imu.setDataRate(ADXL343_DATARATE_100_HZ);
+    imu.setDataRate(ADXL343_DATARATE_1600_HZ);
     imu.setRange(ADXL343_RANGE_4_G);
     imu.begin();
 }
 
+unsigned long prevTime = micros();
+
 void loop() {
     sensors_event_t evt;
     imu.getEvent(&evt);
+    if (millis() % 1000 < 5) {
+        Serial.print("t:"); Serial.print(micros() - prevTime); Serial.print('\t');
+        printAccel(&evt);
+    }
+    prevTime = micros();
+}
+
+void printAccel(sensors_event_t *evt) {
+    Serial.print("x:"); Serial.print('\t'); Serial.print(evt->acceleration.x); Serial.print('\t');
+    Serial.print("y:"); Serial.print('\t'); Serial.print(evt->acceleration.y); Serial.print('\t');
+    Serial.print("z:"); Serial.print('\t'); Serial.print(evt->acceleration.z); Serial.print('\n');
 }
